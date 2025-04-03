90fe6653a7567b0ac194d3889295b42c4acf674d
Dan Marchese <dan@marchese.me>
2025-04-02 18:23:56 -0700

    More gracefully handle unknown tools

--- gpt/assistant.py
+++ gpt/assistant.py
@@ -24,22 +24,23 @@ class AIHomeControlAssistant:
     """The assistant for the main prompt of the home."""
 
     def __init__(
-            self,
-            client: AsyncOpenAI,
-            prompt: str,
-            model: str = env_var('GPT_MODEL'),
-            tools: Optional[List[OpenAIFunction]] = None):
+        self,
+        client: AsyncOpenAI,
+        prompt: str,
+        model: str = env_var("GPT_MODEL"),
+        tools: Optional[List[OpenAIFunction]] = None,
+    ):
         self._client = client
         self._model = model
-        self._tool_map = {
-            f.get_name(): f for f in tools} if tools is not None else {}
+        self._tool_map = {f.get_name(): f for f in tools} if tools is not None else {}
 
         # Get the existing assistant or create a new one
-        assistant_id = env_var('ASSISTANT_ID', True)
+        assistant_id = env_var("ASSISTANT_ID", True)
         self._assistant: Optional[Assistant] = None
         if assistant_id is not None:
             self._assistant = aio.run(
-                self._client.beta.assistants.retrieve(assistant_id))
+                self._client.beta.assistants.retrieve(assistant_id)
+            )
             # Update the model if necessary
             if self._assistant is not None:
                 aio.run(
@@ -47,32 +48,27 @@ class AIHomeControlAssistant:
                         assistant_id=self._assistant.id,
                         model=self._model,
                         instructions=prompt,
-                        tools=[f.get_definition() for f in
-                               self._tool_map.values()]
+                        tools=[f.get_definition() for f in self._tool_map.values()],
                     )
                 )
         if self._assistant is None:
             self._assistant = aio.run(
                 self._client.beta.assistants.create(
                     model=self._model,
-                    name='Home Assistant',
+                    name="Home Assistant",
                     instructions=prompt,
-                    tools=[f.get_definition() for f in
-                           self._tool_map.values()]
+                    tools=[f.get_definition() for f in self._tool_map.values()],
                 )
             )
-            print(
-                f"Main home assistant created with id: '{self._assistant.id}'")
+            print(f"Main home assistant created with id: '{self._assistant.id}'")
         if self._assistant is None:
-            raise RuntimeError(
-                "Failed to instantiate assistant for main prompt")
+            raise RuntimeError("Failed to instantiate assistant for main prompt")
 
         # Get the existing thread or create a new one
-        thread_id = env_var('THREAD_ID', True)
+        thread_id = env_var("THREAD_ID", True)
         self._thread: Optional[Thread] = None
         if thread_id is not None:
-            self._thread = aio.run(
-                self._client.beta.threads.retrieve(thread_id))
+            self._thread = aio.run(self._client.beta.threads.retrieve(thread_id))
         if self._thread is None:
             self._thread = aio.run(self._client.beta.threads.create())
             print(f"Main thread created with id: '{self._thread.id}'")
@@ -82,14 +78,14 @@ class AIHomeControlAssistant:
     async def handle_user_message(self, message: str) -> str:
         """Process a single user message and return the result"""
         await self._client.beta.threads.messages.create(
-            self._thread.id, role='user', content=message
+            self._thread.id, role="user", content=message
         )
 
         run = await self._client.beta.threads.runs.create_and_poll(
             thread_id=self._thread.id, assistant_id=self._assistant.id
         )
-        while run.status != 'completed':
-            if run.status == 'requires_action':
+        while run.status != "completed":
+            if run.status == "requires_action":
                 await self._handle_tool_calls(
                     run.id, run.required_action.submit_tool_outputs.tool_calls
                 )
@@ -97,14 +93,25 @@ class AIHomeControlAssistant:
         messages = await self._client.beta.threads.messages.list(self._thread.id)
         return messages.data[0].content[0].text.value
 
-    async def _handle_tool_calls(self, run_id: str, calls: List[RequiredActionFunctionToolCall]):
+    async def _handle_tool_calls(
+        self, run_id: str, calls: List[RequiredActionFunctionToolCall]
+    ):
         outputs = await aio.gather(*[self._handle_tool_call(call) for call in calls])
         await self._client.beta.threads.runs.submit_tool_outputs(
             run_id=run_id, thread_id=self._thread.id, tool_outputs=outputs
         )
 
-    async def _handle_tool_call(self, call: RequiredActionFunctionToolCall) -> JSONObject:
+    async def _handle_tool_call(
+        self, call: RequiredActionFunctionToolCall
+    ) -> JSONObject:
+        if call.function.name not in self._tool_map:
+            return {
+                "tool_call_id": call.id,
+                "output": f"Tool '{call.function.name}' not found",
+            }
         return {
-            'tool_call_id': call.id,
-            'output': await self._tool_map[call.function.name].invoke(call.function.arguments)
+            "tool_call_id": call.id,
+            "output": await self._tool_map[call.function.name].invoke(
+                call.function.arguments
+            ),
         }
